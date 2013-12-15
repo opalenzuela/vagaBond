5d5602adbc15f867ad46a99fafaa7c17357bbafe
opalenzuela <opalenzuela@gmail.com>
2013-12-15 13:39:32 +0100

    Create testdata.js

---
+++ testdata.js
@@ -0,0 +1,33 @@
+
+/* MODELO De prueba */
+var MyObject1 =  {
+	nombre:"Luisa",
+	edad: "28",
+	fechaNacimiento: "1984-12-13",
+	profesional: "false",
+	sexo: "M",
+	gustos: "Comida exótica y viajes",
+	seguros: "2",
+	mascota: "gato",
+	logo: "images/female_user.png",
+	direccion: {
+		codigoPostal: "08080",
+	}
+};
+
+
+/* MODELO De prueba */
+var MyObject2 =  {
+	nombre:"José",
+	edad: "33",
+	fechaNacimiento: "1979-11-03",
+	profesional: "true",
+	sexo: "H",
+	gustos: "Le gusta la tecnología\n y el deporte de montaña.",
+	seguros: "2,3",
+	mascota: "perro",
+	logo: "images/male_user.png",
+	direccion: {
+		codigoPostal: "08191",
+	}
+};
